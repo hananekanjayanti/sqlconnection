fee33f070ac0f534718eed6eb0ead71632c5702f
Hana Nining <hananiningeka69@gmail.com>
2022-09-20 16:06:11 +0700

    git init

---
+++ sqlConnection/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sqlConnection.Models
+{
+   public class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
---
+++ sqlConnection/Program.cs
@@ -0,0 +1,199 @@
+﻿using sqlConnection.Models;
+using System;
+using System.Data.SqlClient;
+
+namespace sqlConnection
+{
+    class Program
+    {
+        SqlConnection sqlConnection;
+
+        string connectionString = "Data Source=DESKTOP-1BC0NAS;Initial Catalog=DTSxMetroData;User ID=userlogin;Password=***********; Integrated Security=True;MultipleActiveResultSets=True;TrustServerCertificate=True";
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+
+            //GetByNIP
+
+            Employee employee = new Employee()
+            {
+                name = "indah"
+            };
+            program.Insert(employee);
+            //program.Remove(Employee);
+
+            program.GetAll();
+
+        }
+
+        void GetAll()
+        {
+            string query = "SELECT * FROM Employee";
+
+            sqlConnection = new SqlConnection(connectionString);
+            SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+
+            try
+            {
+                sqlConnection.Open();
+                using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader())
+                {
+                    if (sqlDataReader.HasRows)
+                    {
+                        while (sqlDataReader.Read())
+                        {
+                            Console.WriteLine(sqlDataReader[0] + " - " + sqlDataReader[1]);
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("No Data Rows");
+                    }
+                    sqlDataReader.Close();
+                }
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException);
+            }
+
+        }
+
+        void GetByid(int id)
+        {
+            string query = "SELECT * FROM Karyawan WHERE Id = @id";
+
+            SqlParameter sqlParameter = new SqlParameter();
+            sqlParameter.ParameterName = "@id";
+            sqlParameter.Value = id;
+
+            sqlConnection = new SqlConnection(connectionString);
+            SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+            sqlCommand.Parameters.Add(sqlParameter);
+
+            try
+            {
+                sqlConnection.Open();
+                using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader())
+                {
+                    if (sqlDataReader.HasRows)
+                    {
+                        while (sqlDataReader.Read())
+                        {
+                            Console.WriteLine(sqlDataReader[0] + " - " + sqlDataReader[1]);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No Data Rows");
+                    }
+                    sqlDataReader.Close();
+                }
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException);
+            }
+        }
+        void Insert(Employee employee)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                SqlTransaction sqlTransaction = sqlConnection.BeginTransaction();
+
+                SqlCommand sqlCommand = sqlConnection.CreateCommand();
+                sqlCommand.Transaction = sqlTransaction;
+
+                SqlParameter sqlParameter = new SqlParameter();
+                sqlParameter.ParameterName = "@name";
+                sqlParameter.Value = employee.name;
+
+                sqlCommand.Parameters.Add(sqlParameter);
+
+                try
+                {
+                    sqlCommand.CommandText = "INSERT INTO Employee " +
+                        "(name) VALUES (@name)";
+                    sqlCommand.ExecuteNonQuery();
+                    sqlTransaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.InnerException);
+                }
+            }
+        }
+
+        //update
+
+        void Update(Employee employee)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                SqlTransaction sqlTransaction = sqlConnection.BeginTransaction();
+
+                SqlCommand sqlCommand = sqlConnection.CreateCommand();
+                sqlCommand.Transaction = sqlTransaction;
+
+                SqlParameter sqlParameter = new SqlParameter();
+                sqlParameter.ParameterName = "@name";
+                sqlParameter.Value = employee.name;
+
+                //sqlCommand.Parameters.U(sqlParameter);
+
+                try
+                {
+                    sqlCommand.CommandText = "ALTER TABLE Employee " +
+                        "(Name) MODIFY (@name)";
+                    sqlCommand.ExecuteNonQuery();
+                    sqlTransaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.InnerException);
+                }
+            }
+        }
+
+
+        //delete
+        void Delete(Employee employee)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                SqlTransaction sqlTransaction = sqlConnection.BeginTransaction();
+
+                SqlCommand sqlCommand = sqlConnection.CreateCommand();
+                sqlCommand.Transaction = sqlTransaction;
+
+                SqlParameter sqlParameter = new SqlParameter();
+                sqlParameter.ParameterName = "@name";
+                sqlParameter.Value = employee.name;
+
+                sqlCommand.Parameters.Remove(sqlParameter);
+
+                try
+                {
+                    sqlCommand.CommandText = "DROP TABLE Employee " +
+                        "(Name) MODIFY (@name)";
+                    sqlCommand.ExecuteNonQuery();
+                    sqlTransaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.InnerException);
+                }
+            }
+        }
+
+
+
+    }
+}
